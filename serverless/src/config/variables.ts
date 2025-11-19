fc453031879c0240f14ccc9e3c30ba49302806fd
SamNewhouse <hello@samnewhouse.co.uk>
2025-11-19 18:04:04 +0000

    Removed JWT_TOKEN

--- serverless/src/config/variables.ts
+++ serverless/src/config/variables.ts
@@ -7,5 +7,4 @@ export const AWS_REGION = process.env.AWS_REGION;
 export const AWS_REGION_FALLBACK = process.env.AWS_REGION_FALLBACK;
 export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 export const DYNAMODB_ENDPOINT = process.env.DYNAMODB_ENDPOINT;
-export const JWT_SECRET = process.env.JWT_SECRET;
 export const STAGE = process.env.STAGE;
